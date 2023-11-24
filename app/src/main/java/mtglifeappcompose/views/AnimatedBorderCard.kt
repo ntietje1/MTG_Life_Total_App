5d9f2ef1f8c270c9a725af77f4c30f9e13438adc
Nick TIetje <tietje.n@northeastern.edu>
2023-11-24 17:37:18 -0500

    Converted animatedbordercard to modifier

--- app/src/main/java/mtglifeappcompose/views/AnimatedBorderCard.kt
+++ app/src/main/java/mtglifeappcompose/views/AnimatedBorderCard.kt
@@ -6,38 +6,45 @@ import androidx.compose.animation.core.animateFloat
 import androidx.compose.animation.core.infiniteRepeatable
 import androidx.compose.animation.core.rememberInfiniteTransition
 import androidx.compose.animation.core.tween
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.draw.drawWithContent
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.BlendMode
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.graphics.drawscope.rotate
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import java.lang.Float.max
 
-// replace outer surface with clipToBounds modifier
-@Composable
-fun AnimatedBorderCard(
-    modifier: Modifier = Modifier,
-    shape: Shape = RoundedCornerShape(30.dp),
-    borderWidth: Dp = 4.dp,
-    gradient: Brush = Brush.sweepGradient(listOf(Color.Magenta, Color.Cyan)),
+fun Modifier.animatedBorderCard(
+    shape: Shape = RoundedCornerShape(size = 0.dp),
+    borderWidth: Dp = 2.dp,
+    gradient: Brush = Brush.sweepGradient(listOf(Color.Gray, Color.White)),
     animationDuration: Int = 10000,
-    content: @Composable (Size) -> Unit
-
-) {
-    val contentSize = remember { mutableStateOf(Size(0f, 0f)) }
+    onCardClick: () -> Unit = {}
+): Modifier = composed {
     val infiniteTransition = rememberInfiniteTransition(label = "Infinite Color Animation")
     val degrees by infiniteTransition.animateFloat(
         initialValue = 0f,
@@ -49,25 +56,31 @@ fun AnimatedBorderCard(
         label = "Infinite Colors"
     )
 
-    Box(
-        modifier = modifier
+    this.then(
+        Modifier
             .clip(shape)
-            .padding(borderWidth)
-            .drawBehind {
-                rotate(degrees = degrees) {
-                    // Calculate the radius based on content size
-                    val radius = (max(size.width, size.height))
-                    drawCircle(
-                        brush = gradient,
-                        radius = radius,
-                        blendMode = BlendMode.SrcIn,
-                    )
-                }
-            }
-    ) {
-        content(contentSize.value)
-    }
+            .clickable { onCardClick() }
+            .then(
+                Modifier
+                    .fillMaxWidth()
+                    .padding(borderWidth)
+                    .drawWithContent {
+                        rotate(degrees = degrees) {
+                            drawCircle(
+                                brush = gradient,
+                                radius = max(size.width, size.height),
+                                blendMode = BlendMode.SrcIn,
+                            )
+                        }
+                        drawContent()
+                    }
+                    .background(Color.Transparent) // Adjust the color as needed
+                    .clip(shape)
+            )
+    )
 }
+
+
 
 
 
