c3012b8817c542b0da616d135b9615457a2dcd3f
Nick TIetje <tietje.n@northeastern.edu>
2023-12-20 00:18:58 -0500

    Upgraded Gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,6 +37,15 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+    lint {
+        // set to true to turn off analysis progress reporting by lint
+        quiet = true
+        // if true, stop the gradle build if errors are found
+        abortOnError = false
+        // if true, only report errors
+        ignoreWarnings = true
+    }
+
     kotlinOptions {
         jvmTarget = "1.8"
     }
