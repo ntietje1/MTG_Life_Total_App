fb9285cd31dc1352e4b7c5bacc5d62dee3e2ef9a
Nick TIetje <tietje.n@northeastern.edu>
2023-12-20 00:20:21 -0500

    Simplified animatedBorderCard

--- app/src/main/java/mtglifeappcompose/composable/AnimatedBorderCard.kt
+++ app/src/main/java/mtglifeappcompose/composable/AnimatedBorderCard.kt
@@ -1,4 +1,4 @@
-package mtglifeappcompose.views
+package mtglifeappcompose.composable
 
 import androidx.compose.animation.core.LinearEasing
 import androidx.compose.animation.core.RepeatMode
@@ -8,32 +8,19 @@ import androidx.compose.animation.core.rememberInfiniteTransition
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.composed
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.draw.drawWithContent
-import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.BlendMode
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.graphics.drawscope.rotate
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import java.lang.Float.max
@@ -47,37 +34,31 @@ fun Modifier.animatedBorderCard(
 ): Modifier = composed {
     val infiniteTransition = rememberInfiniteTransition(label = "Infinite Color Animation")
     val degrees by infiniteTransition.animateFloat(
-        initialValue = 0f,
-        targetValue = 360f,
-        animationSpec = infiniteRepeatable(
+        initialValue = 0f, targetValue = 360f, animationSpec = infiniteRepeatable(
             animation = tween(durationMillis = animationDuration, easing = LinearEasing),
             repeatMode = RepeatMode.Restart
-        ),
-        label = "Infinite Colors"
+        ), label = "Infinite Colors"
     )
 
     this.then(
         Modifier
             .clip(shape)
             .clickable { onCardClick() }
-            .then(
-                Modifier
-                    .fillMaxWidth()
-                    .padding(borderWidth)
-                    .drawWithContent {
-                        rotate(degrees = degrees) {
-                            drawCircle(
-                                brush = gradient,
-                                radius = max(size.width, size.height),
-                                blendMode = BlendMode.SrcIn,
-                            )
-                        }
-                        drawContent()
+            .then(Modifier
+                .fillMaxWidth()
+                .padding(borderWidth)
+                .drawWithContent {
+                    rotate(degrees = degrees) {
+                        drawCircle(
+                            brush = gradient,
+                            radius = max(size.width, size.height),
+                            blendMode = BlendMode.SrcIn,
+                        )
                     }
-                    .background(Color.Transparent) // Adjust the color as needed
-                    .clip(shape)
-            )
-    )
+                    drawContent()
+                }
+                .background(Color.Transparent) // Adjust the color as needed
+                .clip(shape)))
 }
 
 
