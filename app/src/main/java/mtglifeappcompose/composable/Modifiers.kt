aae4105adb36c06c8bceb8fcf0b451839894f573
Nick TIetje <tietje.n@northeastern.edu>
2023-12-20 23:41:23 -0500

    Reworked repeating clickable

---
+++ app/src/main/java/mtglifeappcompose/composable/Modifiers.kt
@@ -0,0 +1,149 @@
+package mtglifeappcompose.composable
+
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.spring
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.awaitEachGesture
+import androidx.compose.foundation.gestures.awaitFirstDown
+import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.gestures.waitForUpOrCancellation
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberUpdatedState
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.layout.IntrinsicMeasurable
+import androidx.compose.ui.layout.IntrinsicMeasureScope
+import androidx.compose.ui.layout.LayoutModifier
+import androidx.compose.ui.layout.Measurable
+import androidx.compose.ui.layout.MeasureResult
+import androidx.compose.ui.layout.MeasureScope
+import androidx.compose.ui.unit.Constraints
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+enum class ButtonState { Pressed, Idle }
+
+fun Modifier.bounceClick(amount: Float = 0.0075f) = composed {
+    var buttonState by remember { mutableStateOf(ButtonState.Idle) }
+    val scale by animateFloatAsState(
+        targetValue = if (buttonState == ButtonState.Pressed) 1.0f + amount else 1.0f,
+        animationSpec = spring(
+            dampingRatio = Spring.DampingRatioMediumBouncy, stiffness = Spring.StiffnessMedium
+        ),
+        label = "scale"
+    )
+
+    this
+        .graphicsLayer {
+            scaleX = scale
+            scaleY = scale
+        }
+        .clickable(interactionSource = remember { MutableInteractionSource() },
+            indication = null,
+            onClick = { })
+        .pointerInput(buttonState) {
+            awaitPointerEventScope {
+                buttonState = if (buttonState == ButtonState.Pressed) {
+                    waitForUpOrCancellation()
+                    ButtonState.Idle
+                } else {
+                    awaitFirstDown(false)
+                    ButtonState.Pressed
+                }
+            }
+        }
+}
+
+fun Modifier.repeatingClickable(
+    interactionSource: MutableInteractionSource,
+    enabled: Boolean = true,
+    initialDelayMillis: Long = 500,
+    maxDelayMillis: Long = 100,
+    minDelayMillis: Long = 50,
+    delayDecayFactor: Float = 0.0f,
+    onClick: () -> Unit
+): Modifier = composed {
+    val currentClickListener by rememberUpdatedState(onClick)
+    val isEnabled by rememberUpdatedState(enabled)
+
+    pointerInput(Unit) {
+        detectTapGestures(
+            onPress = {
+                onClick()
+            },
+        )
+    }.then(
+        Modifier.pointerInput(interactionSource, isEnabled) {
+            coroutineScope {
+                awaitEachGesture {
+                    val down = awaitFirstDown(requireUnconsumed = false)
+                    val job = launch {
+                        delay(initialDelayMillis)
+                        var currentDelayMillis = maxDelayMillis
+                        while (isEnabled && down.pressed) {
+                            currentClickListener()
+                            delay(currentDelayMillis)
+                            val nextMillis =
+                                currentDelayMillis - (currentDelayMillis * delayDecayFactor)
+                            currentDelayMillis =
+                                nextMillis.toLong().coerceAtLeast(minDelayMillis)
+                        }
+                    }
+                    waitForUpOrCancellation()
+                    job.cancel()
+                }
+            }
+        }
+    )
+}
+
+fun Modifier.rotateVertically(rotation: VerticalRotation) = then(object : LayoutModifier {
+    override fun MeasureScope.measure(
+        measurable: Measurable, constraints: Constraints
+    ): MeasureResult {
+        val placeable = measurable.measure(constraints)
+        return layout(placeable.height, placeable.width) {
+            placeable.place(
+                x = -(placeable.width / 2 - placeable.height / 2),
+                y = -(placeable.height / 2 - placeable.width / 2)
+            )
+        }
+    }
+
+    override fun IntrinsicMeasureScope.minIntrinsicHeight(
+        measurable: IntrinsicMeasurable, width: Int
+    ): Int {
+        return measurable.maxIntrinsicWidth(width)
+    }
+
+    override fun IntrinsicMeasureScope.maxIntrinsicHeight(
+        measurable: IntrinsicMeasurable, width: Int
+    ): Int {
+        return measurable.maxIntrinsicWidth(width)
+    }
+
+    override fun IntrinsicMeasureScope.minIntrinsicWidth(
+        measurable: IntrinsicMeasurable, height: Int
+    ): Int {
+        return measurable.minIntrinsicHeight(height)
+    }
+
+    override fun IntrinsicMeasureScope.maxIntrinsicWidth(
+        measurable: IntrinsicMeasurable, height: Int
+    ): Int {
+        return measurable.maxIntrinsicHeight(height)
+    }
+}).then(rotate(rotation.value))
+
+enum class VerticalRotation(val value: Float) {
+    CLOCKWISE(90f), COUNTER_CLOCKWISE(270f)
+}
