ff07c130cd2cb032926f6380397074b37864e621
Nick TIetje <tietje.n@northeastern.edu>
2023-12-26 01:26:12 -0500

    implemented shared viewmodel across navhost

--- app/src/main/java/mtglifeappcompose/composable/AppContent.kt
+++ app/src/main/java/mtglifeappcompose/composable/AppContent.kt
@@ -1,25 +1,32 @@
 package mtglifeappcompose.composable
 
+import androidx.activity.compose.BackHandler
 import androidx.compose.animation.EnterTransition
 import androidx.compose.animation.ExitTransition
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.compose.ui.platform.LocalView
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.get
+import androidx.navigation.NavBackStackEntry
+import androidx.navigation.NavController
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import mtglifeappcompose.composable.lifecounter.LifeCounterScreen
 import mtglifeappcompose.data.AppViewModel
-import mtglifeappcompose.data.PlayerDataManager
+import mtglifeappcompose.data.SharedPreferencesManager
 import mtglifeappcompose.ui.theme.MTGLifeAppComposeTheme
 
 
@@ -32,42 +39,59 @@ fun MTGLifeTotalApp(
     navController: NavHostController = rememberNavController()
 ) {
 
-//    val initialTheme = isSystemInDarkTheme()
-
-    var darkTheme by remember { mutableStateOf(true) }
-    var numPlayers by remember { mutableStateOf(0) }
-    PlayerDataManager.initialize(LocalContext.current)
+    SharedPreferencesManager.initialize(LocalContext.current)
+    var darkTheme by remember { mutableStateOf(SharedPreferencesManager.loadTheme()) }
 
     MTGLifeAppComposeTheme(darkTheme = darkTheme) {
+        KeepScreenOn()
         NavHost(
             navController = navController,
             startDestination = MTGScreen.PlayerSelectScreen.name,
             enterTransition = { EnterTransition.None },
             exitTransition = { ExitTransition.None },
         ) {
-            composable(route = MTGScreen.PlayerSelectScreen.name) {
-                PlayerSelectScreenWrapper(
-                    setPlayerNum = { numPlayers = it },
-                    goToLifeCounter = { navController.navigate(MTGScreen.LifeCounterScreen.name) }
-                )
+            composable(route = MTGScreen.PlayerSelectScreen.name) { backStackEntry ->
+                val viewModel: AppViewModel = backStackEntry.parentViewModel(navController)
+                PlayerSelectScreenWrapper(setPlayerNum = { viewModel.setPlayerNum(it) }, goToLifeCounter = { navController.navigate(MTGScreen.LifeCounterScreen.name) })
             }
-            composable(route = MTGScreen.LifeCounterScreen.name) {
-                val viewModel: AppViewModel = viewModel()
+            composable(route = MTGScreen.LifeCounterScreen.name) { backStackEntry ->
+                BackHandler(enabled = true) {}
+                val viewModel: AppViewModel = backStackEntry.parentViewModel(navController)
                 viewModel.generatePlayers()
-                viewModel.setPlayerNum(numPlayers, false)
-
                 Surface(Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {}
-                LifeCounterScreen(
-                    players = remember { viewModel.getActivePlayers() },
-                    resetPlayers = {
-                        viewModel.resetPlayers()
-                        navController.navigate(MTGScreen.LifeCounterScreen.name)
-                    },
-                    setStartingLife = { viewModel.setStartingLife(PlayerDataManager, it) },
-                    setPlayerNum = { viewModel.setPlayerNum(it) },
-                    goToPlayerSelect = { navController.navigate(MTGScreen.PlayerSelectScreen.name) },
-                    toggleTheme = { darkTheme = !darkTheme })
+                LifeCounterScreen(players = remember { viewModel.getActivePlayers() }, resetPlayers = {
+                    viewModel.resetPlayers()
+                    navController.navigate(MTGScreen.LifeCounterScreen.name)
+                }, setStartingLife = { viewModel.setStartingLife(SharedPreferencesManager, it) }, setPlayerNum = {
+                    viewModel.setPlayerNum(it, allowOverride = true)
+                }, goToPlayerSelect = { navController.navigate(MTGScreen.PlayerSelectScreen.name) }, set4PlayerLayout = {
+                    viewModel.set4PlayerLayout(it)
+                    navController.navigate(MTGScreen.LifeCounterScreen.name)
+                }, toggleTheme = {
+                    darkTheme = !darkTheme
+                    SharedPreferencesManager.saveTheme(darkTheme)
+                })
             }
         }
     }
+}
+
+@Composable
+fun KeepScreenOn() {
+    val currentView = LocalView.current
+    DisposableEffect(Unit) {
+        currentView.keepScreenOn = true
+        onDispose {
+            currentView.keepScreenOn = false
+        }
+    }
+}
+
+@Composable
+inline fun <reified VM : ViewModel> NavBackStackEntry.parentViewModel(
+    navController: NavController
+): VM {
+    val parentId = destination.parent!!.id
+    val parentBackStackEntry = navController.getBackStackEntry(parentId)
+    return ViewModelProvider(parentBackStackEntry).get()
 }
