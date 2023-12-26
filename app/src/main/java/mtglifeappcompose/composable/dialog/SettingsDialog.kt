13e8c28acc28744017b75e49541aac63b7f53d2a
Nick TIetje <tietje.n@northeastern.edu>
2023-12-26 01:19:11 -0500

    Added general settings dialog

---
+++ app/src/main/java/mtglifeappcompose/composable/dialog/SettingsDialog.kt
@@ -0,0 +1,35 @@
+package mtglifeappcompose.composable.dialog
+
+import androidx.compose.foundation.layout.BoxWithConstraints
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+fun SettingsDialogContent(
+    modifier: Modifier = Modifier, onDismiss: () -> Unit
+) {
+    BoxWithConstraints(modifier) {
+        Text(modifier = Modifier.align(Alignment.Center), text = "To be implemented!")
+
+        /**
+         * TODO: Implement SettingsDialogContent
+         * - Add a SettingsButton for each of the following:
+         *  - GENERAL -
+         *  - Icon?
+         *  - Rotating middle button
+         *  - Self commander damage
+         *  - Disable camera roll
+         *  - Disable haptic feedback
+         *  - Disable screen timeout
+         *
+         *  -
+         *  - CONTACT -
+         *  - Submit Feedback
+         *  - Write a Review
+         *  - About me
+         */
+
+    }
+}
