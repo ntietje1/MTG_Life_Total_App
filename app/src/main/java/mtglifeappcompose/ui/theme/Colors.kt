1f20881b85ce4401306526dff957c155b485bbfa
Nick TIetje <tietje.n@northeastern.edu>
2023-12-31 02:42:17 -0500

    removed unused code

--- app/src/main/java/mtglifeappcompose/ui/theme/Colors.kt
+++ app/src/main/java/mtglifeappcompose/ui/theme/Colors.kt
@@ -6,15 +6,6 @@ import androidx.compose.ui.graphics.ColorMatrix
 import androidx.core.graphics.ColorUtils
 import java.lang.Float.min
 
-
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
-
 val White = Color(0xFFFFFFFF)
 val Black = Color(0xFF000000)
 val Gold = Color(255, 191, 8)
