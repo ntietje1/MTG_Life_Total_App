4a31cc65158b80c6ade508d2a4bdc0d9f28d4146
Nick TIetje <tietje.n@northeastern.edu>
2024-01-02 01:29:06 -0500

    reduced minsdk to 24

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
 
     defaultConfig {
         applicationId = "com.hypeapps.lifelinked"
-        minSdk = 31
+        minSdk = 24
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
