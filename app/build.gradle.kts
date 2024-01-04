4ca282daf3633a76261cad32f6c67a7aeca16c5a
Nick TIetje <tietje.n@northeastern.edu>
2024-01-04 02:19:27 -0500

    updated version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.hypeapps.lifelinked"
         minSdk = 24
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 3
+        versionName = "1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
