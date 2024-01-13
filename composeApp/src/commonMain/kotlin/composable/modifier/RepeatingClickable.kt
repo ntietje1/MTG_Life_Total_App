a7ff097dacb5d8539b7f9f86dcba71ed2c93f8ce
Nick TIetje <tietje.n@northeastern.edu>
2024-01-12 21:42:33 -0500

    Added emitting of gestures (fixed ripple effects)

--- composeApp/src/commonMain/kotlin/composable/modifier/RepeatingClickable.kt
+++ composeApp/src/commonMain/kotlin/composable/modifier/RepeatingClickable.kt
@@ -5,10 +5,13 @@ import androidx.compose.foundation.gestures.awaitFirstDown
 import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.gestures.waitForUpOrCancellation
 import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.PressInteraction
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.composed
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.input.pointer.pointerInput
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.delay
@@ -37,28 +40,40 @@ fun Modifier.repeatingClickable(
     val currentClickListener by rememberUpdatedState(onPress)
     val isEnabled by rememberUpdatedState(enabled)
 
+    val indicationScope = rememberCoroutineScope()
+    var press = PressInteraction.Press(Offset.Zero)
+
     pointerInput(Unit) {
         detectTapGestures(onPress = {
             currentClickListener()
+            indicationScope.launch {
+                press = PressInteraction.Press(it)
+                interactionSource.emit(press)
+            }
         })
-    }.then(Modifier.pointerInput(interactionSource, isEnabled) {
-        coroutineScope {
-            awaitEachGesture {
-                val down = awaitFirstDown(requireUnconsumed = false)
-                val job = launch {
-                    delay(initialDelayMillis)
-                    var currentDelayMillis = maxDelayMillis
-                    while (isEnabled && down.pressed) {
-                        currentClickListener()
-                        delay(currentDelayMillis)
-                        val nextMillis =
-                            currentDelayMillis - (currentDelayMillis * delayDecayFactor)
-                        currentDelayMillis = nextMillis.toLong().coerceAtLeast(minDelayMillis)
+    }.then(
+        Modifier.pointerInput(interactionSource, isEnabled) {
+            coroutineScope {
+                awaitEachGesture {
+                    val down = awaitFirstDown(requireUnconsumed = false)
+                    val job = launch {
+                        delay(initialDelayMillis)
+                        var currentDelayMillis = maxDelayMillis
+                        while (isEnabled && down.pressed) {
+                            currentClickListener()
+                            delay(currentDelayMillis)
+                            val nextMillis =
+                                currentDelayMillis - (currentDelayMillis * delayDecayFactor)
+                            currentDelayMillis = nextMillis.toLong().coerceAtLeast(minDelayMillis)
+                        }
+                    }
+                    waitForUpOrCancellation()
+                    job.cancel()
+                    indicationScope.launch {
+                        interactionSource.emit(PressInteraction.Release(press))
                     }
                 }
-                waitForUpOrCancellation()
-                job.cancel()
             }
         }
-    })
+    )
 }
