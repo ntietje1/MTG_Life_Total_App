63b35745225b9e3a4b9531b972fa85a7db56a335
Nick TIetje <tietje.n@northeastern.edu>
2024-01-12 21:46:32 -0500

    .

--- composeApp/src/iosMain/kotlin/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/MainViewController.kt
@@ -1,3 +1,10 @@
 import androidx.compose.ui.window.ComposeUIViewController
+import androidx.compose.ui.window.ComposeUIViewController
+import platform.UIKit.UIViewController
 
 fun MainViewController() = ComposeUIViewController { App() }
+
+//fun MainViewController(): UIViewController =
+//    ComposeUIViewController {
+//        ImageViewerIos()
+//    }
