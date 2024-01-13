6f310be0acb800ecf5ef579cd8fef47296a44f88
Nick TIetje <tietje.n@northeastern.edu>
2024-01-12 21:48:54 -0500

    redesigned counter dialog

--- composeApp/src/commonMain/kotlin/composable/dialog/CounterDialog.kt
+++ composeApp/src/commonMain/kotlin/composable/dialog/CounterDialog.kt
@@ -1,6 +1,7 @@
 package composable.dialog
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -11,6 +12,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -19,6 +21,8 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.hapticfeedback.HapticFeedbackType
 import androidx.compose.ui.layout.ContentScale
@@ -26,10 +30,11 @@ import androidx.compose.ui.platform.LocalHapticFeedback
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import composable.modifier.bounceClick
 import composable.modifier.repeatingClickable
-import theme.scaledSp
 import org.jetbrains.compose.resources.ExperimentalResourceApi
 import org.jetbrains.compose.resources.painterResource
+import theme.scaledSp
 
 
 /**
@@ -45,18 +50,18 @@ fun CounterDialogContent(
 
     val haptic = LocalHapticFeedback.current
 
-    val imageResources = arrayListOf(
-        painterResource("w_icon.xml"),
-        painterResource("u_icon.xml"),
-        painterResource("b_icon.xml"),
-        painterResource("r_icon.xml"),
-        painterResource("g_icon.xml"),
-        painterResource("c_icon.xml"),
-        painterResource("storm_icon.xml")
+    val counterResources = arrayListOf(
+        Triple(painterResource("w_icon.xml"), Color(0xFFfffbd5), Color(0xFF211d15)),
+        Triple(painterResource("u_icon.xml"), Color(0xFFaae0fa), Color(0xFF061922)),
+        Triple(painterResource("b_icon.xml"), Color(0xFFcbc2bf), Color(0xFF130c0e)),
+        Triple(painterResource("r_icon.xml"), Color(0xFFf9aa8f), Color(0xFF200000)),
+        Triple(painterResource("g_icon.xml"), Color(0xFF9bd3ae), Color(0xFF00160b)),
+        Triple(painterResource("c_icon.xml"), Color(0xFFccc2c0), Color(0xFF130c0e)),
+        Triple(painterResource("storm_icon.xml"), Color(0xFFffd84c), Color(0xFF2b2515))
     )
 
     LazyColumn(
-        modifier = modifier, verticalArrangement = Arrangement.spacedBy(3.dp, Alignment.CenterVertically), horizontalAlignment = Alignment.CenterHorizontally
+        modifier = modifier, verticalArrangement = Arrangement.spacedBy(5.dp, Alignment.CenterVertically), horizontalAlignment = Alignment.CenterHorizontally
     ) {
         item {
             Text(
@@ -66,7 +71,7 @@ fun CounterDialogContent(
 
         items(counters.size - 1) { index ->
             SingleCounter(
-                imageResource = imageResources[index], counter = counters[index]
+                imageResource = counterResources[index].first, backgroundColor = counterResources[index].second, buttonColor = counterResources[index].third, counter = counters[index]
             )
         }
 
@@ -78,7 +83,7 @@ fun CounterDialogContent(
 
         item {
             SingleCounter(
-                imageResource = imageResources.last(), counter = counters.last()
+                imageResource = counterResources.last().first, backgroundColor = counterResources.last().second, buttonColor = counterResources.last().third, counter = counters.last()
             )
         }
 
@@ -102,7 +107,11 @@ fun CounterDialogContent(
 @OptIn(ExperimentalResourceApi::class)
 @Composable
 fun SingleCounter(
-    modifier: Modifier = Modifier, imageResource: Painter = painterResource("placeholder_icon.xml"), counter: MutableIntState
+    modifier: Modifier = Modifier,
+    backgroundColor: Color = Color.Transparent,
+    buttonColor: Color = Color.White,
+    imageResource: Painter = painterResource("placeholder_icon.xml"),
+    counter: MutableIntState
 ) {
 
     val interactionSource = remember { MutableInteractionSource() }
@@ -119,13 +128,19 @@ fun SingleCounter(
     }
 
     Row(
-        modifier = modifier, verticalAlignment = Alignment.CenterVertically, horizontalArrangement = Arrangement.spacedBy(8.dp)
+        modifier = modifier.bounceClick(0.01f).background(backgroundColor, RoundedCornerShape(10.dp)),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.spacedBy(8.dp)
     ) {
         Box(
             modifier = Modifier.size(60.dp).repeatingClickable(interactionSource = interactionSource, enabled = true, onPress = { onDecrement() }),
         ) {
             Image(
-                modifier = Modifier.fillMaxSize(0.7f).align(Alignment.CenterEnd), painter = painterResource("minus_icon.xml"), contentScale = ContentScale.Crop, contentDescription = null
+                modifier = Modifier.fillMaxSize(0.7f).align(Alignment.CenterEnd),
+                painter = painterResource("minus_icon.xml"),
+                contentScale = ContentScale.Crop,
+                contentDescription = null,
+                colorFilter = ColorFilter.tint(buttonColor)
             )
         }
 
@@ -138,12 +153,7 @@ fun SingleCounter(
         Spacer(modifier = Modifier.width(5.dp))
 
         Text(
-            modifier = Modifier.width(50.dp),
-            text = "${counter.value}",
-            textAlign = TextAlign.Justify,
-            color = MaterialTheme.colorScheme.onPrimary,
-            fontSize = 28.scaledSp,
-            fontWeight = FontWeight.Bold
+            modifier = Modifier.width(50.dp), text = "${counter.value}", textAlign = TextAlign.Justify, color = buttonColor, fontSize = 28.scaledSp, fontWeight = FontWeight.Bold
         )
 
         Spacer(modifier = Modifier.width(0.dp))
@@ -152,7 +162,11 @@ fun SingleCounter(
             modifier = Modifier.size(60.dp).repeatingClickable(interactionSource = interactionSource, enabled = true, onPress = { onIncrement() }),
         ) {
             Image(
-                modifier = Modifier.fillMaxSize(0.7f).align(Alignment.CenterStart), painter = painterResource("plus_icon.xml"), contentScale = ContentScale.Crop, contentDescription = null
+                modifier = Modifier.fillMaxSize(0.7f).align(Alignment.CenterStart),
+                painter = painterResource("plus_icon.xml"),
+                contentScale = ContentScale.Crop,
+                contentDescription = null,
+                colorFilter = ColorFilter.tint(buttonColor)
             )
         }
     }
