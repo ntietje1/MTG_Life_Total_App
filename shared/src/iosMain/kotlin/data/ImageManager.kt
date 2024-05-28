d6182553ffd8108fccf526050b1881f1c5a15bf2
ntietje1 <tietje.n@northeastern.edu>
2024-05-28 14:37:40 -0700

    fix image manager for ios!!

--- shared/src/iosMain/kotlin/data/ImageManager.kt
+++ shared/src/iosMain/kotlin/data/ImageManager.kt
@@ -1,9 +1,14 @@
 package data
 
 import androidx.compose.runtime.Composable
+import kotlinx.cinterop.BetaInteropApi
 import kotlinx.cinterop.ExperimentalForeignApi
+import kotlinx.cinterop.addressOf
+import kotlinx.cinterop.usePinned
 import kotlinx.coroutines.Dispatchers
 import platform.Foundation.*
+import platform.UIKit.UIImage
+import platform.UIKit.UIImagePNGRepresentation
 
 actual class ImageManager {
     @OptIn(ExperimentalForeignApi::class)
@@ -23,8 +28,24 @@ actual class ImageManager {
 
     }
 
+    @OptIn(ExperimentalForeignApi::class, BetaInteropApi::class)
     actual suspend fun copyImageToLocalStorage(bytes: ByteArray, fileName: String): String {
-        //TODO:
-        return "yo"
+        val documentsDir = NSSearchPathForDirectoriesInDomains(
+            NSDocumentDirectory, NSUserDomainMask, true
+        ).firstOrNull() ?: throw IllegalStateException("Document directory not found.")
+
+        val destinationPath = "$documentsDir/$fileName"
+
+        val nsData = bytes.usePinned { pinned ->
+            NSData.create(bytes = pinned.addressOf(0), length = bytes.size.toULong())
+        }
+
+        val image = UIImage.imageWithData(nsData)
+        val imageData = image?.let { UIImagePNGRepresentation(it) }
+
+        imageData?.writeToFile(destinationPath, atomically = true)
+            ?: throw IllegalStateException("Failed to write image data to file.")
+
+        return destinationPath
     }
 }
