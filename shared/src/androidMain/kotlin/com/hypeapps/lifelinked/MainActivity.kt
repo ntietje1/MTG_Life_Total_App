e3605b56c87ec6ab1eafafe751b7f233248c2d58
Nicholas Tietje <tietje.n@northeastern.edu>
2024-05-31 20:39:53 -0400

    formatting

--- shared/src/androidMain/kotlin/com/hypeapps/lifelinked/MainActivity.kt
+++ shared/src/androidMain/kotlin/com/hypeapps/lifelinked/MainActivity.kt
@@ -37,7 +37,8 @@ class MainActivity : ComponentActivity() {
                 }
             } else {
                 @Suppress("DEPRECATION")
-                window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                window.decorView.systemUiVisibility = (
+                        View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
                         or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                         or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
                         or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
