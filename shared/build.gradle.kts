76e709bf0dc1053f236cfb45d573e7015976472f
Nicholas Tietje <tietje.n@northeastern.edu>
2024-08-18 20:46:45 -0400

    update version and patchnotes

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -109,8 +109,8 @@ android {
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
         compileSdk = libs.versions.android.compileSdk.get().toInt()
-        versionCode = 10
-        versionName = "1.7.1"
+        versionCode = 11
+        versionName = "1.8.0"
     }
     packaging {
         resources {
