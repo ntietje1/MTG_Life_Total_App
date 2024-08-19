c882017a86fa895d115b528b08025e6e2a3113ee
ntietje1 <tietje.n@northeastern.edu>
2024-08-19 05:24:46 -0700

    comment out non-functioning code

--- shared/src/iosMain/kotlin/Utils.ios.kt
+++ shared/src/iosMain/kotlin/Utils.ios.kt
@@ -29,7 +29,7 @@ actual fun keepScreenOn(keepScreenOn: Boolean) {
     }
 }
 
-class NotificationManager() {
+actual class NotificationManager() {
     actual fun showNotification(message: String, duration: Long) {
 //    val alertController = UIAlertController.alertControllerWithTitle(
 //        title = "Notification",
@@ -48,29 +48,29 @@ class NotificationManager() {
 //        animated = true,
 //        completion = null
 //    )
-        val alertController = UIAlertController.alertControllerWithTitle(
-            title = "Notification",
-            message = message,
-            preferredStyle = UIAlertControllerStyle.Alert
-        )
-        alertController.addAction(
-            UIAlertAction.actionWithTitle(
-                title = "OK",
-                style = UIAlertActionStyle.Default,
-                handler = null
-            )
-        )
-        viewController.presentViewController(
-            viewControllerToPresent = alertController,
-            animated = true,
-            completion = null
-        )
-
-        // Dismiss the alert after the specified duration
-        val dispatchTime = dispatch_time(DISPATCH_TIME_NOW, duration * 1_000_000) // convert to nanoseconds
-        dispatch_after(dispatchTime, dispatch_get_main_queue()) {
-            alertController.dismissViewControllerAnimated(true, null)
-        }
+//        val alertController = UIAlertController.alertControllerWithTitle(
+//            title = "Notification",
+//            message = message,
+//            preferredStyle = UIAlertControllerStyle.Alert
+//        )
+//        alertController.addAction(
+//            UIAlertAction.actionWithTitle(
+//                title = "OK",
+//                style = UIAlertActionStyle.Default,
+//                handler = null
+//            )
+//        )
+//        viewController.presentViewController(
+//            viewControllerToPresent = alertController,
+//            animated = true,
+//            completion = null
+//        )
+//
+//        // Dismiss the alert after the specified duration
+//        val dispatchTime = dispatch_time(DISPATCH_TIME_NOW, duration * 1_000_000) // convert to nanoseconds
+//        dispatch_after(dispatchTime, dispatch_get_main_queue()) {
+//            alertController.dismissViewControllerAnimated(true, null)
+//        }
     }
 }
 
