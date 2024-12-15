142691e6e58f1c79ebb4b9bd6590c418ab3db1d8
Nick <tietje.n@northeastern.edu>
2024-12-15 01:37:18 -0500

    improve color dialog

--- shared/src/commonMain/kotlin/ui/dialog/color/ColorDialog.kt
+++ shared/src/commonMain/kotlin/ui/dialog/color/ColorDialog.kt
@@ -1,14 +1,15 @@
 package ui.dialog.color
 
 
+import PhysicsDraggable
 import androidx.compose.foundation.Canvas
-import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -30,7 +31,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
-import androidx.compose.ui.graphics.BlendMode
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.drawscope.Stroke
@@ -38,6 +38,7 @@ import androidx.compose.ui.graphics.luminance
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.min
+import data.Player
 import lifelinked.shared.generated.resources.Res
 import lifelinked.shared.generated.resources.checkmark
 import lifelinked.shared.generated.resources.x_icon
@@ -46,36 +47,30 @@ import org.koin.compose.koinInject
 import theme.scaledSp
 import theme.toHsv
 import ui.SettingsButton
-import ui.dialog.SettingsDialog
+import ui.dialog.customization.PlayerButtonPreview
+import ui.lifecounter.playerbutton.PBState
 import ui.modifier.routePointerChangesTo
 
-@Composable
-fun ColorDialog(
-    modifier: Modifier = Modifier, onDismiss: () -> Unit, initialColor: Color, setColor: (Color) -> Unit
-) {
-    SettingsDialog(
-        modifier = modifier,
-        backButtonEnabled = false,
-        onDismiss = onDismiss
-    ) {
-        ColorPickerDialogContent(modifier = Modifier.fillMaxSize(), title = "remove this", initialColor = initialColor, setColor = setColor, onDone = onDismiss)
-    }
-}
-
-//TODO: color picker doesn't work when tapping, only dragging
 @Composable
 fun ColorPickerDialogContent(
     modifier: Modifier = Modifier,
     title: String,
     initialColor: Color,
     setColor: (Color) -> Unit = {},
+    initialPlayer: Player,
+    updatePlayerColor: (Color) -> Player,
     onDone: () -> Unit,
     viewModel: ColorDialogViewModel = koinInject()
 ) {
     val state by viewModel.state.collectAsState()
+    var previewPlayer by remember { mutableStateOf(initialPlayer) }
 
-    LaunchedEffect(Unit) {
+    LaunchedEffect(initialColor) {
         viewModel.init(initialColor)
+    }
+
+    LaunchedEffect(state.newColor) {
+        previewPlayer = updatePlayerColor(state.newColor)
     }
 
     BoxWithConstraints(Modifier.wrapContentSize()) {
@@ -84,6 +79,7 @@ fun ColorPickerDialogContent(
         val buttonSize = remember(Unit) { min(maxWidth / 3.5f, maxHeight / 6f) }
         val barHeight = remember(Unit) { maxWidth / 12f }
         val titleSize = remember(Unit) { (maxWidth / 40f + maxHeight / 60f).value }
+        val playerButtonPreviewHeight = remember(Unit) { min(maxWidth / 2f, maxHeight / 3f) }
 
         Column(
             modifier = modifier, horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center
@@ -95,27 +91,48 @@ fun ColorPickerDialogContent(
                 modifier = Modifier.wrapContentSize(), text = title, fontSize = titleSize.scaledSp, color = MaterialTheme.colorScheme.onPrimary
             )
 
-            Spacer(modifier = Modifier.height(padding * 1.5f))
+            Spacer(modifier = Modifier.height(padding))
 
-            SatValPanel(
-                modifier = Modifier.size(largePanelSize),
-                hue = state.hue,
-                initialSaturation = initialColor.toHsv()[1],
-                initialValue = initialColor.toHsv()[2],
-                posn = state.satValPosn,
-                setPosn = { posn -> viewModel.setSatValPosn(posn) },
-            ) { sat, value ->
-                viewModel.setSaturation(sat)
-                viewModel.setValue(value)
+            PhysicsDraggable(
+                Modifier.height(playerButtonPreviewHeight).aspectRatio(1.75f)
+            ) {
+                PlayerButtonPreview(
+                    modifier = Modifier.fillMaxSize(),
+                    name = previewPlayer.name,
+                    lifeNumber = 40,
+                    state = PBState.NORMAL,
+                    isDead = false,
+                    imageUri = null,
+                    backgroundColor = previewPlayer.color,
+                    accentColor = previewPlayer.textColor,
+                )
             }
 
             Spacer(modifier = Modifier.height(padding))
 
             HueBar(
                 modifier = Modifier.width(largePanelSize).height(barHeight),
-                initialHue = initialColor.toHsv()[0]
-            ) { hue ->
-                viewModel.setHue(hue)
+                color = state.newColor
+            ) { value ->
+                viewModel.setHue(value)
+            }
+
+            Spacer(modifier = Modifier.height(padding))
+
+            SatBar(
+                modifier = Modifier.width(largePanelSize).height(barHeight),
+                color = state.newColor
+            ) { value ->
+                viewModel.setSaturation(value)
+            }
+
+            Spacer(modifier = Modifier.height(padding))
+
+            ValBar(
+                modifier = Modifier.width(largePanelSize).height(barHeight),
+                color = state.newColor
+            ) { value ->
+                viewModel.setValue(value)
             }
 
             Spacer(modifier = Modifier.height(padding))
@@ -164,63 +181,52 @@ fun ColorPickerDialogContent(
 }
 
 @Composable
-fun HueBar(
+fun FloatBar(
     modifier: Modifier = Modifier,
-    initialHue: Float = 0f,
-    setHue: (Float) -> Unit
+    colors: List<Color>,
+    initialValue: Float,
+    setValue: (Float) -> Unit
 ) {
-    var pressOffset by remember { mutableStateOf( 0f) }
-    var hueBarWidth by remember { mutableStateOf(0f) }
-
-    LaunchedEffect(initialHue, hueBarWidth) {
-        if (pressOffset == 0.0f) {
-            val initialOffsetX = (initialHue / 360f) * hueBarWidth
-            pressOffset = initialOffsetX
-            setHue(initialHue)
-        }
+    var barSize by remember { mutableStateOf(Size.Zero) }
+    var pressOffset by remember(barSize) {
+        mutableStateOf(initialValue * barSize.width)
     }
 
-    BoxWithConstraints(
-        modifier = modifier
-    ) {
+    BoxWithConstraints(modifier = modifier) {
+        val pointerStrokeSize = (3.dp + maxWidth / 100).value
+
         Box(
             modifier = Modifier
                 .fillMaxSize()
                 .clip(RoundedCornerShape(50))
                 .pointerInput(Unit) {
-                    detectDragGestures { change, _ ->
-                        val x = change.position.x
-                        pressOffset = x
-                    }
-                }
-        ) {
-            Canvas(
-                modifier = Modifier
-                    .fillMaxSize()
-            ) {
-                hueBarWidth = size.width
-                val selectedHue = pressOffset * 360f / size.width
-                setHue(selectedHue)
-
-                val hueColors = List(size.width.toInt()) { index ->
-                    val hue = 360f * index / size.width.toInt()
-                    Color.hsv(hue, 1f, 1f)
-                }
-
-                val rectWidth = size.width / hueColors.size
-                for (i in hueColors.indices) {
-                    drawRect(
-                        color = hueColors[i],
-                        topLeft = Offset(i * rectWidth, 0f),
-                        size = Size(rectWidth, size.height)
+                    routePointerChangesTo(
+                        onMove = { change, _ ->
+                            pressOffset = change.position.x.coerceIn(barSize.height / 8f, barSize.width - barSize.height / 8f)
+                            setValue(pressOffset / barSize.width)
+                        },
+                        onDown = {
+                            pressOffset = it.position.x.coerceIn(barSize.height / 8f, barSize.width - barSize.height / 8f)
+                            setValue(pressOffset / barSize.width)
+                        }
                     )
                 }
+        ) {
+            Canvas(modifier = Modifier.fillMaxSize()) {
+                barSize = size
 
+                // Draw the gradient
+                drawRect(
+                    brush = Brush.horizontalGradient(colors),
+                    size = size
+                )
+
+                // Draw the selector
                 drawCircle(
                     color = Color.White,
-                    radius = size.height / 2,
+                    radius = size.height / 2 - pointerStrokeSize / 2,
                     center = Offset(pressOffset, size.height / 2),
-                    style = Stroke(width = 2.dp.toPx())
+                    style = Stroke(width = pointerStrokeSize)
                 )
             }
         }
@@ -228,97 +234,74 @@ fun HueBar(
 }
 
 @Composable
-fun SatValPanel(
+fun HueBar(
     modifier: Modifier = Modifier,
-    hue: Float = 0f,
-    initialSaturation: Float = 1f,
-    initialValue: Float = 1f,
-    posn: Offset = Offset(0f, 0f),
-    setPosn: (Offset) -> Unit = {},
-    setSatVal: (Float, Float) -> Unit
+    color: Color,
+    setHue: (Float) -> Unit
 ) {
-//    println("GOT POSN: $posn")
-    BoxWithConstraints(
-        Modifier.wrapContentSize()
-    ) {
-        val maxWidth = maxWidth
-        val maxHeight = maxHeight
-        fun updatePosn(x: Float, y: Float) {
-            setPosn(Offset(x, y))
-            val sat = x / (maxWidth.value*1.15f) / 2
-            val value = 1f - y / (maxHeight.value*1.30f)
-            setSatVal(sat, value)
-        }
-//        val size = Size(maxWidth.value, maxHeight.value)
-        Box(
-            modifier = Modifier
-                .wrapContentSize()
-                .clip(RoundedCornerShape(5))
-                .pointerInput(Unit) {
-                    routePointerChangesTo(
-                        onDown = {
-                            updatePosn(it.position.x, it.position.y)
-                        },
-                        onMove = { pointerInputChange, _ ->
-                            updatePosn(pointerInputChange.position.x, pointerInputChange.position.y)
-                        }
-                    )
-                }
-        ) {
-            LaunchedEffect(initialValue, initialSaturation, posn) {
-                if (posn == Offset.Zero) {
-                    val x = initialSaturation * maxWidth.value*2*1.15f
-                    val y = (1f - initialValue) * maxHeight.value*1.30f
-                    setPosn(Offset(x, y))
-                }
-            }
-            val satShader = remember(hue) {
-                Brush.horizontalGradient(
-                    colors = listOf(Color.Transparent, Color.hsv(hue, 1f, 1f)),
-                    startX = 0f,
-                    endX = maxWidth.value*2
-                )
-            }
-            val valShader = remember {
-                Brush.verticalGradient(
-                    colors = listOf(Color.White, Color.Black),
-                    startY = 0f,
-                    endY = maxHeight.value*1.30f
-                )
-            }
-
-            Canvas(
-                modifier = modifier
-            ) {
-                drawRect(
-                    color = Color.White,
-                    topLeft = Offset.Zero,
-                    size = size
-                )
-                drawRect(
-                    brush = valShader,
-                    topLeft = Offset.Zero,
-                    size = size,
-                )
-                drawRect(
-                    brush = satShader,
-                    topLeft = Offset.Zero,
-                    size = size,
-                    blendMode = BlendMode.Multiply
-                )
-
-                drawCircle(
-                    color = Color.White, radius = 8.dp.toPx(), center = posn, style = Stroke(
-                        width = 2.dp.toPx()
-                    )
-                )
-
-                drawCircle(
-                    color = Color.White,
-                    radius = 2.dp.toPx(),
-                    center = posn,
-                )
-            }
-        }
+    val colors = remember {
+        listOf(
+            Color.hsv(0f, 1f, 1f),    // Red
+            Color.hsv(60f, 1f, 1f),   // Yellow
+            Color.hsv(120f, 1f, 1f),  // Green
+            Color.hsv(180f, 1f, 1f),  // Cyan
+            Color.hsv(240f, 1f, 1f),  // Blue
+            Color.hsv(300f, 1f, 1f),  // Magenta
+            Color.hsv(360f, 1f, 1f)   // Red
+        )
     }
+
+    val (hue, _, _) = color.toHsv()
+    FloatBar(
+        modifier = modifier,
+        colors = colors,
+        initialValue = hue / 360f,
+        setValue = {
+            setHue(it * 360f)
+        }
+    )
+}
+
+@Composable
+fun SatBar(
+    modifier: Modifier = Modifier,
+    color: Color,
+    setSaturation: (Float) -> Unit
+) {
+    val (hue, saturation, value) = color.toHsv()
+    val colors = remember(hue, value) {
+        listOf(
+            Color.hsv(hue, 0f, 1f),
+            Color.hsv(hue, 1f, 1f)
+        )
+    }
+
+    FloatBar(
+        modifier = modifier,
+        colors = colors,
+        initialValue = saturation,
+        setValue = setSaturation
+    )
+}
+
+@Composable
+fun ValBar(
+    modifier: Modifier = Modifier,
+    color: Color,
+    setValue: (Float) -> Unit
+) {
+    val (hue, saturation, value) = color.toHsv()
+    val colors = remember(hue, saturation) {
+        listOf(
+            Color.hsv(hue, 1f, 0f),
+            Color.hsv(hue, 1f, 1f)
+        )
+    }
+
+    FloatBar(
+        modifier = modifier,
+        colors = colors,
+        initialValue = value,
+        setValue = setValue
+    )
 }
