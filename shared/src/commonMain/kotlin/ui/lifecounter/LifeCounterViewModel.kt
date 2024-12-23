c158d59fa683643e5874d1465777a1617cc8a57d
Nick <tietje.n@northeastern.edu>
2024-12-22 20:34:30 -0500

    add check for timer state

--- shared/src/commonMain/kotlin/ui/lifecounter/LifeCounterViewModel.kt
+++ shared/src/commonMain/kotlin/ui/lifecounter/LifeCounterViewModel.kt
@@ -127,6 +127,7 @@ open class LifeCounterViewModel(
     }
 
     fun killTimer() {
+        setFirstPlayerSelectionActive(false)
         timerJob?.cancel()
         timerJob = null
         updatePlayerButtonTimers(-1, null)
