d2ea15fd585bbb033479889f4d90e055f850d321
Nicholas Tietje <tietje.n@northeastern.edu>
2026-07-01 14:28:49 -0400

    upgrade version and add sync task

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,20 +11,60 @@ plugins {
 val releasePropertiesFile = layout.projectDirectory.file("release.properties")
 val iosProjectFile = layout.projectDirectory.file("iosApp/LifeLinkedIOS.xcodeproj/project.pbxproj")
 
+fun loadReleaseProperties(): java.util.Properties {
+    return java.util.Properties().apply {
+        releasePropertiesFile.asFile.inputStream().use(::load)
+    }
+}
+
+fun java.util.Properties.requiredReleaseProperty(name: String): String {
+    return requireNotNull(getProperty(name)) {
+        "Missing $name in release.properties"
+    }
+}
+
+fun replaceIosVersionSetting(iosProject: String, name: String, value: String): String {
+    val setting = Regex("""$name = [^;]+;""")
+    require(setting.containsMatchIn(iosProject)) {
+        "Missing $name in ${iosProjectFile.asFile.path}"
+    }
+    return setting.replace(iosProject, "$name = $value;")
+}
+
+val syncReleaseVersion by tasks.registering {
+    inputs.file(releasePropertiesFile)
+    outputs.file(iosProjectFile)
+
+    doLast {
+        val releaseProperties = loadReleaseProperties()
+        val versionCode = releaseProperties.requiredReleaseProperty("versionCode")
+        val versionName = releaseProperties.requiredReleaseProperty("versionName")
+
+        var iosProject = iosProjectFile.asFile.readText()
+        iosProject = replaceIosVersionSetting(
+            iosProject = iosProject,
+            name = "CURRENT_PROJECT_VERSION",
+            value = versionCode
+        )
+        iosProject = replaceIosVersionSetting(
+            iosProject = iosProject,
+            name = "MARKETING_VERSION",
+            value = versionName
+        )
+
+        iosProjectFile.asFile.writeText(iosProject)
+    }
+}
+
 tasks.register("verifyReleaseVersion") {
     inputs.file(releasePropertiesFile)
     inputs.file(iosProjectFile)
+    mustRunAfter(syncReleaseVersion)
 
     doLast {
-        val releaseProperties = java.util.Properties().apply {
-            releasePropertiesFile.asFile.inputStream().use(::load)
-        }
-        val versionCode = requireNotNull(releaseProperties.getProperty("versionCode")) {
-            "Missing versionCode in release.properties"
-        }
-        val versionName = requireNotNull(releaseProperties.getProperty("versionName")) {
-            "Missing versionName in release.properties"
-        }
+        val releaseProperties = loadReleaseProperties()
+        val versionCode = releaseProperties.requiredReleaseProperty("versionCode")
+        val versionName = releaseProperties.requiredReleaseProperty("versionName")
         val iosProject = iosProjectFile.asFile.readText()
 
         require("CURRENT_PROJECT_VERSION = $versionCode;" in iosProject) {
